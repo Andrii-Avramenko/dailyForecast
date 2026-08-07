31be9848af1b035a6f7d24d46ea63cc5bcdec2b1
Andrii Avramenko <andreitvmm228@gmail.com>
2026-08-07 17:32:51 +0100

    Імпорт модалки

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero/Hero";
 import Forecast from "./components/Forecast/Forecast";
 import Gallery from "./components/Gallery/Gallery";
 import Footer from "./components/Footer/Footer";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   return (
