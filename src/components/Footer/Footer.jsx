c0abf39044c3e791a0d505ec9a6f696a1b318a0c
kotle9ka <popovkolya81010@gmail.com>
2026-08-11 18:29:14 +0400

    footer upd1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,53 @@
+import {
+    Container,
+    ContentWrapper,
+    Logo,
+    Section,
+    Title,
+    AddressText,
+    SocialList,
+    SocialItem,
+    SocialIcon,
+  } from './Footer.styled';
+
 function Footer() {
     return (
         <>
-        
+    <Container>
+      <ContentWrapper>
+        <Logo src="../Footer/Images/logo.png" alt="24/7 Forecast" />
+
+        <Section>
+          <Title>Address</Title>
+          <AddressText>
+            Svobody str. 35<br />
+            Kyiv<br />
+            Ukraine
+          </AddressText>
+        </Section>
+
+        <Section>
+          <Title>Contact us</Title>
+          <SocialList>
+            <SocialItem>
+              <a href="#"  rel="">
+                <SocialIcon src="../Footer/Images/Instagram.png" alt="Instagram" />
+              </a>
+            </SocialItem>
+            <SocialItem>
+              <a href="#"  rel="">
+                <SocialIcon src="./Images/Facebook.png" alt="Facebook" />
+              </a>
+            </SocialItem>
+            <SocialItem>
+              <a href="#"  rel="">
+                <SocialIcon src="./Images/whatsapp.png" alt="WhatsApp" />
+              </a>
+            </SocialItem>
+          </SocialList>
+        </Section>
+      </ContentWrapper>
+    </Container>
         </>
     )
 }
