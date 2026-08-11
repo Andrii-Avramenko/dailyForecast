515036cd2f4b5d97bd0c4244d8e3dba8378f917e
lev-voronovych <vlev0760@gmail.com>
2026-08-11 14:53:49 +0300

    додав папку services та розмітку для Gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import fetchGalleryPhotos from "../../services/galleryPhotos";
+
 function Gallery() {
-    return (
-        <>
-        
-        </>
-    )
+  const [photos, setPhotos] = useState([]);
+
+  async function fetchPhotos() {
+    try {
+      const data = await fetchGalleryPhotos("nature", 1);
+      setPhotos(data.hits ?? []);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <>
+      <h2>Beautiful nature</h2>
+      <ul>
+        {photos.map(({ id, webformatURL }) => (
+          <li key={id}>
+            <img src={webformatURL} alt="nature" />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default Gallery
+export default Gallery;
