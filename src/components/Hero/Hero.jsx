f4f7dad076f9711c96231e53ca1b615c36094ddd
ivan <vavrynka.vistak@gmail.com>
2026-08-12 19:32:45 +0200

    1

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 function Hero() {
+  const [] = useState()  
+  const [] = useState()  
+  const [] = useState()  
+  const [] = useState()  
     return (
         <>
-
+            <h1></h1>
+            <p></p>
+            <input type="text" />
+            <button></button>
         </>
     )
 }
