af6d859e05350136a5a38405ae2a6cce67042769
Andrii Avramenko <andreitvmm228@gmail.com>
2026-08-15 23:09:58 +0100

    Повернув футер

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero/Hero";
 import Forecast from "./components/Forecast/Forecast";
 import Gallery from "./components/Gallery/Gallery";
 import News from "./components/News/News";
-// import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 import Modal from "./components/Modal/Modal";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       <Forecast />
       <News />
       <Gallery />
-      {/* <Footer /> */}
+      <Footer />
       <GlobalStyle />
     </>
   );
