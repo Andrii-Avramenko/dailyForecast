2b228613d8187ade3632940fd1c50bc9986a235c
Andrii Avramenko <andreitvmm228@gmail.com>
2026-08-15 23:05:56 +0100

    Логіка username в хедері

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "../../assets/logo.webp";
 import user from "../../assets/header/user.png";
 import Modal from "../Modal/Modal";
 
 import { StyledHeader, Navigation, UserInfo, SignUp } from "./Header.styled";
 
-export default function Header({ name = "" }) {
+export default function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [username, setUsername] = useState(() => localStorage.getItem('username') ?? '')
+
+  const updateName = (name) => {
+    localStorage.setItem('username', name)
+    setUsername(name)
+  }
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -36,11 +42,12 @@ export default function Header({ name = "" }) {
           </ul>
         </Navigation>
         <UserInfo>
-          <SignUp type="button" onClick={openModal}>Sign Up</SignUp>
+          {!username && <SignUp type="button" onClick={openModal}>Sign Up</SignUp>}
+          {!!username && <p>{username}</p>}
           <img src={user} alt="user icon" />
         </UserInfo>
       </StyledHeader>
-      {isModalOpen && <Modal onClose={closeModal} />}
+      {isModalOpen && <Modal onClose={closeModal} updateName={updateName}/>}
     </>
   );
 }
