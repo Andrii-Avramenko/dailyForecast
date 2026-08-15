21be7bb9461987fc2d93155f1133db77adcc84a9
ivan <vavrynka.vistak@gmail.com>
2026-08-15 20:40:46 +0200

    3

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react"
 import { input } from "./Hero.styled.jsx"
+import { HiOutlineSearch } from "react-icons/hi";
 
 function Hero() {
   const [searching, setSearching] = useState("");  
   const [] = useState() 
+
+  function handleChange(event) {
+    setSearching(event.target.value);
+  }
     return (
       <>
         <h1>Weather dashboard</h1>
@@ -13,12 +18,14 @@ function Hero() {
         </p>
         <p></p>
         <input
-
+          onChange={handleChange}
           value={searching}
           type="text"
           placeholder="Search location..."
         />
-        <button></button>
+        <button>
+          <HiOutlineSearch />
+        </button>
       </>
     );
 }
--- src/components/Hero/Hero.styled.jsx
+++ src/components/Hero/Hero.styled.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
 export const input = styled.input`
-  border: 1px solid brown;
+  &:focus {
+    outline: none;
+  }
 `;
