300d11600b814b4f32f9d31892c94a330ef31588
ivan <vavrynka.vistak@gmail.com>
2026-08-15 20:14:31 +0200

    2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react"
+import { input } from "./Hero.styled.jsx"
 
 function Hero() {
-  const [] = useState()  
-  const [] = useState()  
-  const [] = useState()  
-  const [] = useState()  
+  const [searching, setSearching] = useState("");  
+  const [] = useState() 
     return (
-        <>
-            <h1></h1>
-            <p></p>
-            <input type="text" />
-            <button></button>
-        </>
-    )
+      <>
+        <h1>Weather dashboard</h1>
+        <p>
+          Create your personal list of favorite cities and always be aware of
+          the weather.
+        </p>
+        <p></p>
+        <input
+
+          value={searching}
+          type="text"
+          placeholder="Search location..."
+        />
+        <button></button>
+      </>
+    );
 }
 
 export default Hero
---
+++ src/components/Hero/Hero.styled.jsx
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+export const input = styled.input`
+  border: 1px solid brown;
+`;
